0772468a575c3f31445570c7605c8035e8c329f3
Vi Cruz <virmelcruz@gmail.com>
2017-12-07 16:48:26 +0800

    Follows eslint coding style

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 'use strict'
 
-const express = require('express');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
-const app = express();
+const express = require('express')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpack = require('webpack')
+const webpackConfig = require('./webpack.config')
+const app = express()
 
-const compiler = webpack(webpackConfig);
+const compiler = webpack(webpackConfig)
 const PORT = process.env.PORT || 3000
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
@@ -19,11 +19,11 @@ app.use(webpackDevMiddleware(compiler, {
     colors: true,
   },
   historyApiFallback: true
-}));
+}))
 
-app.use(require("webpack-hot-middleware")(compiler));
+app.use(require("webpack-hot-middleware")(compiler))
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
-});
+})
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import './sample.scss';
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import './sample.scss'
 
 ReactDOM.render(
   <AppContainer>
     <div> 123 </div>
   </AppContainer>,
   document.getElementById('root')
-);
+)
 
 if (module.hot) {
   module.hot.accept()
